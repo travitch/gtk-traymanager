1720ff8cbf433cf1753f4034e16861a216ba10a3
Alexey Kotlyarov <a@koterpillar.com>
2016-05-19 18:07:10 +1000

    Port to GI and Gtk 3

--- src/Graphics/UI/Gtk/Misc/TrayManager.hs
+++ src/Graphics/UI/Gtk/Misc/TrayManager.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE ForeignFunctionInterface, EmptyDataDecls #-}
+{-# LANGUAGE ScopedTypeVariables, CPP, OverloadedStrings, NegativeLiterals, ConstraintKinds, TypeFamilies, MultiParamTypeClasses, KindSignatures, FlexibleInstances, UndecidableInstances, DataKinds, FlexibleContexts #-}
 -- | This module implements a TrayManager - an integral part of a
 -- Linux system tray widget, though it is not itself a widget.  This
 -- package exports a single GObject (for use with gtk2hs) that
@@ -33,11 +33,11 @@
 module Graphics.UI.Gtk.Misc.TrayManager (
   -- * Types
   TrayManager,
-  TrayManagerChild,
-  TrayManagerClass,
-  castToTrayManager,
+  TrayManagerK,
   toTrayManager,
-  gTypeTrayManager,
+  noTrayManager,
+
+  TrayManagerChild,
 
   -- * Functions
   trayManagerCheckRunning,
@@ -46,87 +46,86 @@ module Graphics.UI.Gtk.Misc.TrayManager (
   trayManagerGetChildTitle,
 
   -- * Signals
-  trayIconAdded,
-  trayIconRemoved,
-  trayMessageSent,
-  trayMessageCanceled,
-  trayLostSelection
+  onTrayIconAdded,
+  -- TODO
+  -- onTrayIconRemoved,
+  -- onTrayMessageSent,
+  -- onTrayMessageCanceled,
+  -- onTrayLostSelection
   ) where
 
-import Graphics.UI.Gtk hiding ( after )
-import Graphics.UI.Gtk.Abstract.Object ( makeNewObject )
-import Graphics.UI.GtkInternals
-import System.Glib.GError ( failOnGError )
-import System.Glib.GType
-import System.Glib.UTFString ( peekUTFString )
+import Prelude ()
+import Data.GI.Base.ShortPrelude
 
-import Control.Monad ( liftM )
+import qualified Data.Text as T
+
+import qualified GI.GObject as GObject
+
+import GI.Gdk.Objects.Screen
+
+import GI.Gtk
 
 import Foreign
-import Foreign.C.String ( CString, peekCString )
 import Foreign.C.Types
-import Foreign.ForeignPtr.Unsafe as UF
 import Unsafe.Coerce ( unsafeCoerce )
 
 newtype TrayManager = TrayManager (ForeignPtr TrayManager)
-                    deriving (Eq, Ord)
+foreign import ccall "egg_tray_manager_get_type"
+  c_egg_tray_manager_get_type :: IO GType
+
+type instance ParentTypes TrayManager = TrayManagerParentTypes
+type TrayManagerParentTypes = '[Widget, GObject.Object]
+
+instance GObject TrayManager where
+    gobjectIsInitiallyUnowned _ = True
+    gobjectType _ = c_egg_tray_manager_get_type
+
+class GObject o => TrayManagerK o
+instance (GObject o, IsDescendantOf TrayManager o) => TrayManagerK o
+
+toTrayManager :: TrayManagerK o => o -> IO TrayManager
+toTrayManager = unsafeCastTo TrayManager
+
+noTrayManager :: Maybe TrayManager
+noTrayManager = Nothing
+
+foreign import ccall "egg_tray_manager_new"
+  egg_tray_manager_new :: IO (Ptr TrayManager)
+
+trayManagerNew :: MonadIO m => m TrayManager
+trayManagerNew = liftIO $ do
+    result <- egg_tray_manager_new
+    checkUnexpectedReturnNULL "egg_tray_manager_new" result
+    result' <- (newObject TrayManager) result
+    return result'
+
+--------------------
+
 type TrayManagerChild = Ptr EggTrayManagerChild
 
 -- Empty data tags to classify some foreign pointers
-data EggTrayManager
 data EggTrayManagerChild
 
 foreign import ccall "egg_tray_manager_check_running"
   c_egg_tray_manager_check_running :: Ptr Screen -> IO CInt
 
-trayManagerCheckRunning :: Screen -> IO Bool
-trayManagerCheckRunning gdkScreen = do
+trayManagerCheckRunning :: MonadIO m => Screen -> m Bool
+trayManagerCheckRunning gdkScreen = liftIO $ do
   let ptrScreen = unsafeCoerce gdkScreen :: ForeignPtr Screen
   withForeignPtr ptrScreen $ \realPtr -> do
     res <- c_egg_tray_manager_check_running realPtr
     return (res /= 0)
 
-mkTrayManager :: (ForeignPtr TrayManager -> TrayManager, FinalizerPtr a)
-mkTrayManager = (TrayManager, objectUnrefFromMainloop)
-
-unTrayManager :: TrayManager -> ForeignPtr TrayManager
-unTrayManager (TrayManager o) = o
-
-class GObjectClass o => TrayManagerClass o
-toTrayManager :: TrayManagerClass o => o -> TrayManager
-toTrayManager = unsafeCastGObject . toGObject
-
-instance TrayManagerClass TrayManager
-instance ObjectClass TrayManager
-instance GObjectClass TrayManager where
-  toGObject = GObject . castForeignPtr . unTrayManager
-  unsafeCastGObject = TrayManager . castForeignPtr . unGObject
-
-castToTrayManager :: GObjectClass o => o -> TrayManager
-castToTrayManager = castTo gTypeTrayManager "TrayManager"
-
-foreign import ccall "egg_tray_manager_get_type"
-  c_egg_tray_manager_get_type :: CULong
-
-gTypeTrayManager :: GType
-gTypeTrayManager = fromIntegral c_egg_tray_manager_get_type
-
-foreign import ccall "egg_tray_manager_new"
-  c_egg_tray_manager_new :: IO (Ptr EggTrayManager)
-
-trayManagerNew :: IO TrayManager
-trayManagerNew =
-  makeNewObject mkTrayManager $ liftM (castPtr :: Ptr EggTrayManager -> Ptr TrayManager) $
-    c_egg_tray_manager_new
-  -- tm <- c_egg_tray_manager_new
-  -- return (unsafeCoerce tm)
-
 foreign import ccall "egg_tray_manager_manage_screen"
-  c_egg_tray_manager_manage_screen :: Ptr EggTrayManager -> Ptr Screen -> IO CInt
+  c_egg_tray_manager_manage_screen :: Ptr TrayManager -> Ptr Screen -> IO CInt
 
-trayManagerManageScreen :: TrayManager -> Screen -> IO Bool
-trayManagerManageScreen trayManager screen = do
-  let ptrManager = unsafeCoerce trayManager :: ForeignPtr EggTrayManager
+trayManagerManageScreen ::
+    (MonadIO m, TrayManagerK a) =>
+    a
+    -> Screen
+    -> m Bool
+trayManagerManageScreen trayManager screen = liftIO $ do
+  let ptrManager = unsafeCoerce trayManager :: ForeignPtr TrayManager
       ptrScreen = unsafeCoerce screen :: ForeignPtr Screen
   res <- withForeignPtr ptrManager $ \realManager -> do
     withForeignPtr ptrScreen $ \realScreen -> do
@@ -134,22 +133,57 @@ trayManagerManageScreen trayManager screen = do
   return (res /= 0)
 
 foreign import ccall "egg_tray_manager_get_child_title"
-  c_egg_tray_manager_get_child_title :: Ptr EggTrayManager -> Ptr EggTrayManagerChild -> IO (Ptr CChar)
+  c_egg_tray_manager_get_child_title :: Ptr TrayManager -> Ptr EggTrayManagerChild -> IO (Ptr CChar)
 
 
-trayManagerGetChildTitle :: TrayManager -> TrayManagerChild -> IO String
+trayManagerGetChildTitle :: TrayManager -> TrayManagerChild -> IO T.Text
 trayManagerGetChildTitle trayManager child = do
-  let ptrManager = unsafeCoerce trayManager :: ForeignPtr EggTrayManager
+  let ptrManager = unsafeCoerce trayManager :: ForeignPtr TrayManager
   res <- withForeignPtr ptrManager $ \realManager -> do
     c_egg_tray_manager_get_child_title realManager child
-  peekCString res
+  cstringToText res
 
 -- | The signal emitted when a new tray icon is added.  These are
 -- delivered even for systray icons that already exist when the tray
 -- manager is created.
-trayIconAdded :: (TrayManagerClass self) => Signal self (Widget -> IO ())
-trayIconAdded = Signal (connect_OBJECT__NONE "tray_icon_added")
 
+type TrayIconAddedCallback = Widget -> IO ()
+
+type TrayIconAddedCallbackC =
+    Ptr () ->
+    Ptr Widget ->
+    Ptr () ->
+    IO ()
+
+foreign import ccall "wrapper"
+    mkTrayIconAddedCallback :: TrayIconAddedCallbackC -> IO (FunPtr TrayIconAddedCallbackC)
+
+trayIconAddedClosure :: TrayIconAddedCallback -> IO Closure
+trayIconAddedClosure cb = newCClosure =<< mkTrayIconAddedCallback wrapped
+    where wrapped = trayIconAddedCallbackWrapper cb
+
+trayIconAddedCallbackWrapper ::
+    TrayIconAddedCallback ->
+    Ptr () ->
+    Ptr Widget ->
+    Ptr () ->
+    IO ()
+trayIconAddedCallbackWrapper _cb _ object _ = do
+    object' <- (newObject Widget) object
+    _cb  object'
+
+onTrayIconAdded :: (GObject a, MonadIO m) => a -> TrayIconAddedCallback -> m SignalHandlerId
+onTrayIconAdded obj cb = liftIO $ connectTrayIconAdded obj cb SignalConnectBefore
+afterTrayIconAdded :: (GObject a, MonadIO m) => a -> TrayIconAddedCallback -> m SignalHandlerId
+afterTrayIconAdded obj cb = connectTrayIconAdded obj cb SignalConnectAfter
+
+connectTrayIconAdded :: (GObject a, MonadIO m) =>
+                         a -> TrayIconAddedCallback -> SignalConnectMode -> m SignalHandlerId
+connectTrayIconAdded obj cb after = liftIO $ do
+    cb' <- mkTrayIconAddedCallback (trayIconAddedCallbackWrapper cb)
+    connectSignalFunPtr obj "tray_icon_added" cb' after
+
+{-
 -- | This signal is emitted when a tray icon is removed by its parent
 -- application.  No action is really necessary here (the icon is
 -- removed without any intervention).  You could do something here if
@@ -171,70 +205,4 @@ trayMessageCanceled = Signal (connect_OBJECT_INT64__NONE "message_canceled")
 -- | ??
 trayLostSelection :: (TrayManagerClass self) => Signal self (IO ())
 trayLostSelection = Signal (connect_NONE__NONE "lost_selection")
-
-
--- Boilerplate stolen from gtk to make this library compatible.  These
--- functions aren't exported at all so I just copied them.
-
--- stolen from gtk
-castTo :: (GObjectClass obj, GObjectClass obj') => GType -> String -> (obj -> obj')
-castTo gtype objTypeName obj =
-  case toGObject obj of
-    gobj@(GObject objFPtr)
-      | typeInstanceIsA ((UF.unsafeForeignPtrToPtr.castForeignPtr) objFPtr) gtype
-                  -> unsafeCastGObject gobj
-      | otherwise -> error $ "Cannot cast object to " ++ objTypeName
-
-connect_OBJECT__NONE ::
-  (GObjectClass a', GObjectClass obj) => SignalName ->
-  ConnectAfter -> obj ->
-  (a' -> IO ()) ->
-  IO (ConnectId obj)
-connect_OBJECT__NONE signal after obj user =
-  connectGeneric signal after obj action
-  where action :: Ptr GObject -> Ptr GObject -> IO ()
-        action _ obj1 =
-          failOnGError $
-          makeNewGObject (GObject, objectUnrefFromMainloop) (return obj1) >>= \obj1' ->
-          user (unsafeCastGObject obj1')
-
-connect_NONE__NONE ::
-  GObjectClass obj => SignalName ->
-  ConnectAfter -> obj ->
-  (IO ()) ->
-  IO (ConnectId obj)
-connect_NONE__NONE signal after obj user =
-  connectGeneric signal after obj action
-  where action :: Ptr GObject -> IO ()
-        action _ =
-          failOnGError $
-          user
-
-connect_OBJECT_INT64__NONE :: (GObjectClass a', GObjectClass obj)
-                              => SignalName
-                              -> ConnectAfter
-                              -> obj
-                              -> (a' -> Int64 -> IO ())
-                              -> IO (ConnectId obj)
-connect_OBJECT_INT64__NONE signal after obj user =
-  connectGeneric signal after obj action
-  where
-    action :: Ptr GObject -> Ptr GObject -> Int64 -> IO ()
-    action _ obj1 int2 =
-      failOnGError $ makeNewGObject (GObject, objectUnrefFromMainloop) (return obj1) >>= \obj1' ->
-        user (unsafeCastGObject obj1') int2
-
-connect_OBJECT_STRING_INT64_INT64__NONE :: (GObjectClass a', GObjectClass obj)
-                                           => SignalName
-                                           -> ConnectAfter
-                                           -> obj
-                                           -> (a' -> String -> Int64 -> Int64 -> IO ())
-                                           -> IO (ConnectId obj)
-connect_OBJECT_STRING_INT64_INT64__NONE signal after obj user =
-  connectGeneric signal after obj action
-  where
-    action :: Ptr GObject -> Ptr GObject -> CString -> Int64 -> Int64 -> IO ()
-    action _ obj1 str2 int3 int4 =
-      failOnGError $ makeNewGObject (GObject, objectUnrefFromMainloop) (return obj1) >>= \obj1' ->
-        peekUTFString str2 >>= \str2' ->
-          user (unsafeCastGObject obj1') str2' int3 int4
+-}
